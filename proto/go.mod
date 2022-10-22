fcdf6a31c53b1cc033531398c3c3a972ad245b4c
x893675 <x893675@icloud.com>
2022-10-22 18:07:44 +0800

    first commit

---
+++ proto/go.mod
@@ -0,0 +1,3 @@
+module github.com/x893675/malenia/proto
+
+go 1.19
---
+++ sample-server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	daprd "github.com/dapr/go-sdk/service/grpc"
+	helloworldpb "github.com/x893675/malenia/proto/helloworld"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	helloworldpb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &helloworldpb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	srv := grpc.NewServer()
+
+	s := daprd.NewServiceWithListener(lis)
+
+	_ = s.AddServiceInvocationHandler("echo", func(ctx context.Context, in *common.InvocationEvent) (out *common.Content, err error) {
+		log.Printf("in echo handler %v\n", *in)
+		out = &common.Content{
+			Data:        in.Data,
+			ContentType: in.ContentType,
+			DataTypeURL: in.DataTypeURL,
+		}
+		return
+	})
+
+	helloworldpb.RegisterGreeterServer(srv, &server{})
+
+	pb.RegisterAppCallbackServer(srv, s.(*daprd.Server))
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := srv.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
